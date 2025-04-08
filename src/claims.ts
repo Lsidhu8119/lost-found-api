05ca3a77da1b56db0bb9f1f6d13dc7463c0e1d58
Lovedeep Singh Sidhu <sidhulovedeep8119.com>
2025-04-08 13:29:46 -0500

    Created constant of claims in claim.ts

---
+++ src/claims.ts
@@ -0,0 +1,11 @@
+export const claims = [
+    {
+      id: '1',
+      itemId: 'abc123',
+      claimantName: 'Lovedeep Sidhu',
+      claimantEmail: 'Lovedeepsidhu@gmail.com',
+      justification: 'I can identify the engraving inside',
+      dateClaimed: '2024-03-27',
+    },
+  ];
+  
