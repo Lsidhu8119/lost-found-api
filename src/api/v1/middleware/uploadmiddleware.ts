1cbac2cc5b02401f854e9caf64849bdfa38687b0
Lovedeep Singh Sidhu <sidhulovedeep8119.com>
2025-04-25 11:20:25 -0500

    Pushed the readme.md file

---
+++ src/api/v1/middleware/uploadmiddleware.ts
@@ -0,0 +1,24 @@
+import multer from 'multer';
+import path from 'path';
+
+const storage = multer.diskStorage({
+  destination: 'uploads/', // Folder to save images
+  filename: (_req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+export const upload = multer({
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  fileFilter: (_req, file, cb) => {
+    const filetypes = /jpeg|jpg|png/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+    if (extname && mimetype) {
+      cb(null, true);
+    } else {
+      cb(new Error('Images only!'));
+    }
+  },
+});
